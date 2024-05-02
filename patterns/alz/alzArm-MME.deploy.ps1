4fb406e82958ca6a199ac7e64662c860f50627c4
Manu Meyer <manu@grabx.ch>
2024-05-02 10:31:10 +0200

    added params for test tenant mme

---
+++ patterns/alz/alzArm-MME.deploy.ps1
@@ -0,0 +1,56 @@
+
+
+az login --tenant d3450f80-7150-46aa-bfab-194c2ff90391
+
+#************************************************************#
+# Subscription PROD
+
+    az account set -s "711c2059-9e0b-404c-9653-5b31ad49a4fc"
+
+    az account show
+    Set-Location "C:\Users\ManuelMeyer\OneDrive - GrabX Solutions\_Projects\Profond SLA\_Code\amba_monitoring-profond2\patterns\alz"
+    # $template = ".\alzArm-MME.json"
+    $templateUri =  "https://raw.githubusercontent.com/grabx/amba_monitoring-profond2/main/patterns/alz/alzArm-MME.json"
+    $parameterfile = ".\alzArm-MME.param.json"
+    
+    #Validate File
+    az deployment mg validate `
+        --management-group-id "manutenant" `
+        --template-uri  templateUri `
+        --parameters $parameterfile `
+        --location "switzerlandnorth" 
+
+
+
+
+
+    #Deploy Template
+    $deploymentname = $template.Trim('.', '\').Replace('\','_') + "-" + (Get-Date -format yyyyMMddhhmm).ToString()
+    az deployment mg create `
+    --name $deploymentname `
+    --management-group-id "manutenant" `
+    --template-uri  templateUri `
+    --parameters $parameterfile `
+    --location "switzerlandnorth" 
+
+    #--template-file $template `
+     #--template-uri  https://raw.githubusercontent.com/***YourGithubFork***/azure-monitor-baseline-alerts/***main
+
+
+#     az deployment mg create --name "amba-GeneralDeployment" --template-uri  https://raw.githubusercontent.com/***YourGithubFork***/azure-monitor-baseline-alerts/***main
+# or branchname***/patterns/alz/alzArm.json --location $location --management-group-id $pseudoRootManagementGroup --parameters ".\patterns\alz\alzArm.param.json"
+
+
+    # az deployment group create `
+    #     --name $deploymentname `
+    #     --resource-group $resourcegroup `
+    #     --template-file $template `
+    #     --parameters $parameterfile 
+
+
+    # az deployment mg create `
+    #     --name "amba-mme-deployment" `
+    #     --template-file ".\alsArm-MME.json"
+    #     --location "switzerlandnorth" `
+    #     --management-group-id "manutenant" `
+    #     --parameters ".\patterns\alz\alzArm.param.json"
